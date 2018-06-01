7d69d167a4fd9ae4d9b059e5fd4b856a66f635d1
nwactor <nickwactor@gmail.com>
2018-05-31 23:49:51 -0700

    Added settings menu skeleton and sample animation

--- MainScreen.js
+++ MainScreen.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 import AppHeader from './AppHeader';
 import TrainListScroller from './TrainListScroller';
+import SettingsMenu from './SettingsMenu';
 
 class MainScreen extends React.Component {
 	
+    constructor() {
+        super();
+        this.xPosition = new Animated.Value(1);
+    }
+
+    componentDidMount() {
+       this.animateDrawerOpen();
+    }
+
+    animateDrawerOpen() {
+        Animated.timing(
+            this.xPosition,
+            {
+                toValue: 0,
+                duration: 3000
+            }
+        ).start();
+    }
+
+    animateDrawerClose() {
+
+    }
 
 	render() {
         const trains = this.props.trains;
+        const objectMargin = this.xPosition.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0%', '100%']
+        });
+        
+        //there are some stupid hacks needed in life, and this is one of them.
+        //Reason: Animated's interoplate returns the string "0%" instead of 0%
+        const stringMargin = JSON.stringify(objectMargin);
+        const marginRight = stringMargin.slice(1, stringMargin.length - 1);
+        
         return (
             <View style={styles.container}>
                 <AppHeader
@@ -18,6 +51,13 @@ class MainScreen extends React.Component {
                 	trains={trains}
                 	onTrainSelect={this.props.onTrainSelect}
                 />
+                <View style={[
+                        styles.animatedDrawer,
+                        {marginRight: marginRight}
+                    ]}
+                >
+                    <SettingsMenu/>
+                </View>
             </View>
         );
     }
@@ -28,6 +68,11 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#d3d3d3'
     },
+    animatedDrawer: {
+        height: "100%",
+        width: "100%",
+        position: "absolute"
+    }
 });
 
 export default MainScreen;
---
+++ SettingsMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class SettingMenu extends React.Component {
+	render() {
+        return (
+        	<View style={styles.container}>
+        		<Text style={styles.headerText}>Settings</Text>
+        	</View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        width: '100%',
+        // position: 'absolute',
+        backgroundColor: '#524f7a'
+    },
+    headerText: {
+    	color: '#ffd65e'
+    }
+});
